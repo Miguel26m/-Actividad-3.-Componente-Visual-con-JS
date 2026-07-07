14696ca217088015fe87fa03bac05de14413b524
Miguel <22161135@itoaxaca.edu.mx>
2026-07-07 08:50:06 -0600

    index

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const datos = [
+        {img: 'img/rico.jpg', titulo: 'Rico', descripcion: 'Brawler que se caracteriza por sus basicos con rebote' },
+        {img: 'img/brock.jpg', titulo: 'Brock', descripcion: 'Brawler destructor de largo alcance.' },
+        {img: 'img/Colt.jpg', titulo: 'Colt', descripcion: 'Brawler que se caracteriza por tener mucho daño por segundo.' },
+        {img: 'img/8bit.jpg', titulo: '8-Bit', descripcion: 'Brawler que hace mucho daño pero es muy lento al caminar' }
+    ];
+    
+new Carrusel('personajes', datos);
